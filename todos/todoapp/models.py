c92d7d6b57aad901e37f37c7b3f2a7a9ef88ef85
tamaspenzes-2001 <penzestamas@proton.me>
2024-08-08 13:45:06 +0200

    implement models

--- todos/todoapp/models.py
+++ todos/todoapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+  title = models.CharField(max_length=20)
+
+  def get_absolute_url(self):
+    return "/lists"
+
+  def str(self):
+    return self.title
+
+class TodoItem(models.Model):
+  text = models.CharField(max_length=30)
+  checked = models.BooleanField(default=False)
+  todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+
+  def str(self):
+    return self.text
